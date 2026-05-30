7ffb9afd70d6126414b23ba70809be83a3fa39e5
Ben-Keilholz <benjkeilholz@gmail.com>
2026-05-30 02:08:59 -0500

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,4 +5,4 @@ from app.models.menu import Menu, Category, Modifier_Group, Modifier
 from app.models.inventory import Inventory
 from app.models.order import Order, Order_Item, Order_Item_Modifier
 from app.models.payment import Payment, Refund
-from app.models.rbac import Employee, Manager, Admin
+from app.models.rbac import Role, Permission, RolePermission, EmployeeRole
