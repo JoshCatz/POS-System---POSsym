cb048e73b1502521036f1b20188201bdf4374b50
Ben-Keilholz <benjkeilholz@gmail.com>
2026-06-02 15:33:14 -0500

    Update order.py

--- backend/app/models/order.py
+++ backend/app/models/order.py
@@ -11,28 +11,5 @@ class Order(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     restaurant_id: Mapped[int] = mapped_column(ForeignKey("restaurants.id"))
     employee_id: Mapped[int] = mapped_column(ForeignKey("employees.id"))
-    table_id: Mapped[int] = mapped_column(ForeignKey("tables.id"))
-    number_of_guests: Mapped[int] = mapped_column()
+    table_id: Mapped[int] = mapped_column(ForeignKey("tables.id"))       
     paid: Mapped[bool] = mapped_column(default=False)
-
-class Order_Item(Base):
-    __tablename__ = "order_items"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
-    menu_id: Mapped[int] = mapped_column(ForeignKey("menus.id"))
-    seat_num: Mapped[Optional[int]] = mapped_column(nullable=True)
-    quantity: Mapped[int] = mapped_column(default=1)
-    instructions: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
-    discount_id: Mapped[Optional[int]] = mapped_column(ForeignKey("discounts.id"), nullable=True)
-    discount_amount: Mapped[Optional[Decimal]] = mapped_column(Numeric(10,2), nullable=True)
-    voided: Mapped[bool] = mapped_column(default=False)
-    voided_by: Mapped[Optional[int]] = mapped_column(ForeignKey("employees.id"), nullable=True)
-    void_reason: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
-
-class Table(Base):
-
-class Ticket(Base):
-
-
-
