f8f99ee350786429f3bcb06eb9a43a47602f8419
Ben-Keilholz <benjkeilholz@gmail.com>
2026-06-02 22:55:56 -0500

    Created database.py, config.py, and modified .env to include the database URL. Deleted db.py and __init__.py as they are no longer needed.

--- backend/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.models.base import Base
-from app.models.restaurant import Restaurant
-from app.models.employee import Employee, Position, Employee_Position, Shift
-from app.models.menu import Menu, Category, Modifier_Group, Modifier
-from app.models.inventory import Inventory
-from app.models.order import Order, Order_Item, Order_Item_Modifier
-from app.models.payment import Payment, Refund
-from app.models.rbac import Role, Permission, RolePermission, EmployeeRole
